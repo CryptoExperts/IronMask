9684326a7bf7ac056090997eec3b1a2ab76dcd07
DadaIsCrazy <darius.mercadier@gmail.com>
2021-11-16 13:29:55 +0100

    Make IronMask compatible with MacOS X
    get_nprocs was used to get the number of CPU cores, but is
    not available on Mac OS X. This is now fixed.

--- src/config.h
+++ src/config.h
@@ -6,7 +6,6 @@
 #define BATCH_SIZE 1000000 // 1 million
 
 #include <stdint.h>
-#include <sys/sysinfo.h>
 
 #define LARGE_CIRCUITS
 // To support circuits with more than 255 variables, define LARGE_CIRCUITS
@@ -31,5 +30,38 @@
 #endif
 
 
+#ifdef MACOS
+#include <sys/param.h>
+#include <sys/sysctl.h>
+static int __attribute__((unused)) get_number_of_procs() {
+  // Code to get the number of CPUs on MacOS X taken from
+  // https://stackoverflow.com/a/150971/4990392
+  int mib[4];
+  int numCPU;
+  size_t len = sizeof(numCPU);
+
+  /* set the mib for hw.ncpu */
+  mib[0] = CTL_HW;
+  mib[1] = HW_AVAILCPU;  // alternatively, try HW_NCPU;
+
+  /* get the number of CPUs from the system */
+  sysctl(mib, 2, &numCPU, &len, NULL, 0);
+
+  if (numCPU < 1) {
+    mib[1] = HW_NCPU;
+    sysctl(mib, 2, &numCPU, &len, NULL, 0);
+    if (numCPU < 1)
+      numCPU = 1;
+  }
+  return numCPU;
+}
+
+#define CORES_TO_USE_FOR_MULTITHREADING (get_number_of_procs() - 1)
+
+#else // linux
+
+#include <sys/sysinfo.h>
 // Number of cores to use when the "-j -1" option is used
 #define CORES_TO_USE_FOR_MULTITHREADING (get_nprocs() - 1)
+
+#endif
